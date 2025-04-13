74f9cde4467d71d495e3da2f20157785074e16ef
Piyush-2004 <135797444+Piyush-2004@users.noreply.github.com>
2025-04-13 08:45:58 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as a newer version
+
+//alert(3+3) // We are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log('Piyush')     
+
+
+let name = "Piyush"
+let age = 18
+let isLoggedin = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
